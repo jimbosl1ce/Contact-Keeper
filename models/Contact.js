0f2b0535afc54580d5dca9e0d3b253942097f1fd
Jimmy Hogerty <jdhogerty@gmail.com>
2021-08-05 19:20:51 -0700

    Added React in root client folder Set up concurrently in root package.json folder npm run dev now runs MongoDB on port 5000 and React on port 3000 ... cool :)

---
+++ models/Contact.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+  user: {
+    // Here, we associate contact with user...
+    type: mongoose.Schema.Types.ObjectId,
+    // Here, we refer to the specific collection we're referencing, which is 'users'
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'Personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('contact', ContactSchema);
